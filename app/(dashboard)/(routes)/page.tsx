e5b4373034f6d0486b3f494614cdd809f994a882
Hasan Sayyed <hasansayyed.personal@gmail.com>
2024-03-18 16:53:45 +0530

    Added Auth: Using Clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
 export default page;
