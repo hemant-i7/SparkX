2c4961ccc3e48ac64f419f1f90a547694deb054a
Hasan Sayyed <hasansayyed.personal@gmail.com>
2024-03-19 17:29:22 +0530

    Course Creation: /api/courses/[courseId]

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,11 +2,33 @@ import React from "react";
 import { auth } from "@clerk/nextjs";
 import { IconBadge } from "@/components/icon-badge";
 import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import { connect } from "@/lib/db";
+import Course from "@/models/Course";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  connect();
+
   const { userId } = auth();
 
-  const requiredFields = [userId, params.courseId];
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await Course.findOne({ _id: params.courseId });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
 
   const totalFields = requiredFields.length;
 
@@ -15,28 +37,24 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const completionText = `(${completedFields} / ${totalFields})`;
 
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col gap-y-2">
-          <h1 className="text-2xl font-medium">Course setup</h1>
-          <span className="text-sm text-slate-700">
+    <div className='p-6'>
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col gap-y-2'>
+          <h1 className='text-2xl font-medium'>Course setup</h1>
+          <span className='text-sm text-slate-700'>
             Complete all fields {completionText}
           </span>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
         <div>
-          <div className="flex items-center gap-x-2">
+          <div className='flex items-center gap-x-2'>
             <IconBadge icon={LayoutDashboard} />
-            <h2 className="text-xl">Customize your course</h2>
+            <h2 className='text-xl'>Customize your course</h2>
           </div>
-          
-           {/* <TitleForm
-              initialData={null}
-              courseId={.id}
-            /> */}
-           
+
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
